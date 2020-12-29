5de4557620ec630a0ddb33ab4b18a3c87254d887
olivernyc <oliver@olivernyc.net>
2020-12-29 00:31:37 -0500

    Rename var

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 export default function Field({ name, value }) {
-  let label;
+  let formattedValue;
   if (typeof value === "boolean") {
-    label = value ? "Yes" : "No";
+    formattedValue = value ? "Yes" : "No";
   } else {
-    label = value || `No ${name}`;
+    formattedValue = value || `No ${name}`;
   }
   return (
     <div className="mv3">
       <div className="w4 mb1" style={{ minWidth: "8rem" }}>
         <span className="mid-gray ttc">{name}</span>
       </div>
-      <span className="dark-gray">{label}</span>
+      <span className="dark-gray">{formattedValue}</span>
     </div>
   );
 }
