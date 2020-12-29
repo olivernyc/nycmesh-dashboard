06139e29365b77ffa8daaa5d5296d815faf2348f
Oliver <oliver@olivernyc.net>
2020-12-29 00:41:59 -0500

    Various fixes and tweaks (#20)
    * Dark mode
    
    * Various fixes and tweaks
    
    * Rename var
    
    * Show localized date
    
    * Show node install and deactivate dates

--- src/components/MapView/NodeLayer.js
+++ src/components/MapView/NodeLayer.js
@@ -3,17 +3,15 @@ import React from "react";
 import NodeMarker from "./NodeMarker";
 
 function NodeLayer({ nodes, selectedNode, onClick }) {
-	return nodes
-		.filter((node) => node.status === "active")
-		.map((node) => (
-			<NodeMarker
-				key={node.id}
-				node={node}
-				selected={false}
-				dimmed={false}
-				onClick={() => onClick(node)}
-			/>
-		));
+	return nodes.map((node) => (
+		<NodeMarker
+			key={node.id}
+			node={node}
+			selected={false}
+			dimmed={false}
+			onClick={() => onClick(node)}
+		/>
+	));
 }
 
 export default React.memo(NodeLayer);
--- src/components/MapView/RequestLayer.js
+++ src/components/MapView/RequestLayer.js
@@ -3,13 +3,13 @@ import React from "react";
 import RequestMarker from "./RequestMarker";
 
 function RequestLayer({ requests, selectedRequest, selectedNode, onClick }) {
-	return requests.map((request) => (
-		<RequestMarker
-			key={request.id}
-			request={request}
-			onClick={() => onClick(request)}
-		/>
-	));
+  return requests.map((request) => (
+    <RequestMarker
+      key={request.id}
+      request={request}
+      onClick={() => onClick(request)}
+    />
+  ));
 }
 
 export default React.memo(RequestLayer);
