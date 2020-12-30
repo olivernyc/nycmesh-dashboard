638afcb7e31ac659c66437c29be002274e23fb14
David Albert <davidbalbert@gmail.com>
2020-12-30 18:24:00 -0500

    Use Modal

--- src/components/Member/MemberSelect.js
+++ src/components/Member/MemberSelect.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-import Button from "../Button2";
+import Modal from "../Modal";
 import { fetchResource } from "../../api";
 
 export default function MemberSelect(props) {
@@ -34,51 +34,32 @@ export default function MemberSelect(props) {
   }, [isAuthenticated, getAccessTokenSilently])
 
   return (
-    <div className="absolute absolute--fill bg-white-70 z-5">
-      <div
-        className="bg-white mw6 w-100 center shadow br2"
-        style={{ marginTop: "15vh" }}
-      >
-        <div className="pa3 f5 fw5">Add a member</div>
+    <Modal
+      title="Add a member"
+      buttonLabel="Add member"
+      onDone={() => {
+        props.onSubmit(selectedId)
+      }}
+      onCancel={props.onCancel}
+    >
+      <div className="flex flex-column">
+        <label htmlFor="member" className="mb2">Member</label>
+        <div>
+          <select id="member" className="mr2" onChange={handleChange}>
+            {
+              members.map(m =>
+                <option key={m.id} value={m.id}>{m.name} – {m.email}</option>
+              )
+            }
+          </select>
 
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            props.onSubmit(selectedId);
-          }}
-        >
-          <div className="pa3 bg-near-white bt bb b--light-gray f6 fw5 flex flex-column">
-            <label htmlFor="member" className="mb2">Member</label>
-            <div>
-              <select id="member" className="mr2" onChange={handleChange}>
-                {
-                  members.map(m =>
-                    <option key={m.id} value={m.id}>{m.name} – {m.email}</option>
-                  )
-                }
-              </select>
-
-              {
-                loading ?
-                <div>Loading...</div> :
-                null
-              }
-            </div>
-          </div>
-          <div className="pa3 flex justify-end">
-            <div className="ml2">
-              <Button label="Cancel" onClick={props.onCancel} />
-            </div>
-            <div className="ml2">
-              <Button
-                label="Add member"
-                primary
-                type="submit"
-              />
-            </div>
-          </div>
-        </form>
+          {
+            loading ?
+            <div>Loading...</div> :
+            null
+          }
+        </div>
       </div>
-    </div>
-  )
+    </Modal>
+  );
 }
