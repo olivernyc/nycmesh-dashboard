0c9058915e560bb36f1c4b837cfe6a4d9a2027e6
David Albert <davidbalbert@gmail.com>
2020-12-31 10:18:52 -0500

    Style disabled buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+import { classNames } from '../utils';
+
 export default function Button({ type, primary, label, icon, onClick, disabled }) {
+	const classes = classNames("ph2 pv1 ba b--black-20 br2 fw5", {
+		pointer: !disabled,
+		"bg-purple white": primary && !disabled,
+		"bg-purple silver": primary && disabled,
+		"bg-white dark-gray": !primary && !disabled,
+		"bg-white-80 silver": !primary && disabled,
+	});
+
 	return (
 		<button
-			className={`ph2 pv1 ba b--black-20 br2 fw5 pointer ${
-				primary ? "bg-purple white" : "bg-white dark-gray"
-			}`}
+			className={classes}
 			onClick={onClick}
 			type={type || "button"}
 			disabled={disabled}
